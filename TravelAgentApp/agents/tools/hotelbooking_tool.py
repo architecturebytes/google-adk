b0f8f5b46059597b7ecbc386ef1ccd3d7d46bb46
architecturebytes <125813896+architecturebytes@users.noreply.github.com>
2025-05-29 10:23:31 +0530

    Create hotelbooking_tool.py

---
+++ TravelAgentApp/agents/tools/hotelbooking_tool.py
@@ -0,0 +1,4 @@
+def book_hotel_tool(city: str, start_date: str, end_date: str, no_of_guests: int) -> str:
+    # Do your actual hotel booking stuff here.
+    # return "Sorry, no hotels are available on these dates."
+    return "Hotel successfully booked."
