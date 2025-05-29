21751a6a7eada05c40b0287ec48ffc128e99f294
architecturebytes <125813896+architecturebytes@users.noreply.github.com>
2025-05-29 10:23:11 +0530

    Create flightbooking_tool.py

---
+++ TravelAgentApp/agents/tools/flightbooking_tool.py
@@ -0,0 +1,4 @@
+def book_flight_tool(departure_city: str, destination_city: str, travel_date: str, no_of_passengers: int) -> str:
+    # Do your actual flight booking stuff here.
+    #return "Sorry, all flights are full on these dates."
+    return "Flight successfully booked."
